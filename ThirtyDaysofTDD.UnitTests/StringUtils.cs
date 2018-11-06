bc7bfd244efad06c4030dae9259db809ecb23202
surekhasrinivasan <surekhasrao@gmail.com>
2018-11-06 06:43:23 -0500

    Thirty Days of TDD- Day 3 & 4 - First Test and Making your Test Pass

---
+++ ThirtyDaysofTDD.UnitTests/StringUtils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ThirtyDaysofTDD.UnitTests
+{
+    public class StringUtils
+    {
+        public static int FindNumberOfOccurences(string sentenceToScan, string characterToScanFor)
+        {
+            // TODO: Implement this method
+            //throw new NotImplementedException();
+            //return 2;
+
+            var stringToCheckAsCharacterArray = sentenceToScan.ToCharArray();
+            var characterToCheckFor = Char.Parse(characterToScanFor);
+
+            var numberOfOccurenes = 0;
+
+            for (var charIdx = 0; charIdx < stringToCheckAsCharacterArray.GetUpperBound(0); charIdx++)
+            {
+                if (stringToCheckAsCharacterArray[charIdx] == characterToCheckFor)
+                {
+                    numberOfOccurenes++;
+                }
+            }
+
+            return numberOfOccurenes;
+
+        }
+    }
+}
---
+++ ThirtyDaysofTDD.UnitTests/StringUtilsTest.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirtyDaysofTDD.UnitTests
+{
+    [TestFixture]
+    public class StringUtilsTest
+    {
+        [Test]
+        public void ShouldBeAbleToCountNumberOfLettersInSimpleSentence()
+        {
+            var sentenceToScan = "TDD is awesome!";
+
+            var characterToScanFor = "e";
+
+            var expectedResult = 2;
+
+            //var stringUtils = new StringUtils();
+
+            int result = StringUtils.FindNumberOfOccurences(sentenceToScan, characterToScanFor);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void ShouldBeAbleToCountNumberOfLettersInAComplexSentance()
+        {
+            var sentenceToScan = "Once is unique, twice is a coincidence, three times is a pattern.";
+
+            var characterToScanFor = "n";
+
+            var expectedResult = 5;
+
+            //var stringUtils = new StringUtils();
+
+            int result = StringUtils.FindNumberOfOccurences(sentenceToScan, characterToScanFor);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+    }
+}
