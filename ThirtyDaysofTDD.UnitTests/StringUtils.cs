0c9b9102d62e4dae1239cde41f31900fd4743b85
surekhasrinivasan <surekhasrao@gmail.com>
2018-11-10 06:53:49 -0500

    30 Days of TDD - Day Nine - Refactoring Basics

--- ThirtyDaysofTDD.UnitTests/StringUtils.cs
+++ ThirtyDaysofTDD.UnitTests/StringUtils.cs
@@ -6,29 +6,24 @@ namespace ThirtyDaysofTDD.UnitTests
     {
         public static int FindNumberOfOccurences(string sentenceToScan, string characterToScanFor)
         {
-            try
-            {
-                var stringToCheckAsCharacterArray = sentenceToScan.ToCharArray();
-                var characterToCheckFor = Char.Parse(characterToScanFor);
-
-                var numberOfOccurenes = 0;
-
-                for (var charIdx = 0; charIdx < stringToCheckAsCharacterArray.GetUpperBound(0); charIdx++)
-                {
-                    if (stringToCheckAsCharacterArray[charIdx] == characterToCheckFor)
-                    {
-                        numberOfOccurenes++;
-                    }
-                }
-
-                return numberOfOccurenes;
-
-            }
-            catch
+            if(characterToScanFor.Length != 1)
             {
                 throw new ArgumentException();
+            }            
+            var stringToCheckAsCharacterArray = sentenceToScan.ToCharArray();
+            var characterToCheckFor = Char.Parse(characterToScanFor);
+
+            var numberOfOccurenes = 0;
+
+            for (var charIdx = 0; charIdx < stringToCheckAsCharacterArray.GetUpperBound(0); charIdx++)
+            {
+                if (stringToCheckAsCharacterArray[charIdx] == characterToCheckFor)
+                {
+                    numberOfOccurenes++;
+                }
             }
 
+            return numberOfOccurenes;            
         }
     }
 }
